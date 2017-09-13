c57c2a169c0610c3686e4cddd4b888f6d93ff558
kidceg <31203059+kidceg@users.noreply.github.com>
2017-09-13 22:38:36 +0800

    use bootstrap

---
+++ js/Miyazaki.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    // 点击“推荐主题曲”按钮会跳转到对应位置
+	$("#tuijian").click(function () {
+		$("body,html").animate({scrollTop:$(".theme_song").offset().top},200);
+	});
+
+    //点击“推荐电影”下的标签跳转到对应内容
+	$("#navDiv .dropdown-menu a").click(function () {
+		var href = $(this).attr("href");
+		$("#movieName a[href='" + href + "']").tab("show");
+	});
+
+});
