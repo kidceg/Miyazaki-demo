a3857b73238f6c5fda5844cda074aa98d2440e41
kidceg <31203059+kidceg@users.noreply.github.com>
2017-09-18 22:08:49 +0800

    change js

--- js/Miyazaki.js
+++ js/Miyazaki.js
@@ -48,63 +48,63 @@ var pre_audio;
 //用来储存上一个播放的按钮的下标
 var pre_index;
 
-for (var i = 0; i < len; i++) {
-	// 注意！获取宽度，如果是非内联css样式，只能用offsetWidth
-	//进度条的最大长度，整个框架x55%
-	var Pmax = $song[0].offsetWidth * 55 /100;
-	// 当点击播放按钮时
-  $btn_audio.eq(i).on("click", function () {
- 	//用来表示audio的总时间
-	var Atime = 0; 	
-  	// 获取当前节点data-index属性值，和对应audio的ID一样
-  	var audio_id = $(this).attr('data-index');
-  	var x = audio_id.match(/\d/);
-    // 点击时判断是否是三角形播放按钮
-  	if ($(this).hasClass('glyphicon-play') ) {
-	   	//让圆点显示出来
-	   	$yuan.eq(x).css("display","block");
-        // 判断上一个点击的audio，让其暂停，并改变按钮图标
-        if (pre_audio!=undefined) {
-        	pre_audio.pause();      	
-        	$btn_audio.eq(pre_index).removeClass('glyphicon-pause').addClass('glyphicon-play');
-        }
-        //当前符号条件的audio按钮背景换成暂停
-	   	$(this).removeClass('glyphicon-play').addClass('glyphicon-pause');
-		//当前符号条件的audio播放
-	  	$("#" + audio_id)[0].play();	
-        //设置定时器，
-	  	var a = setInterval(function(){
-	  		//当audio暂停的时候，清除定时器
-	  		if ($("#" + audio_id).paused){
-	  			clearInterval(a);
-	  		}
-	  		//定义当前audio已播放时间
-           var Ctime = $("#" + audio_id)[0].currentTime;
-	  		// 让(class为time_count)p标签内容变成当前播放时间
-	  		if (Ctime > 0.01) {
-			  	//Atime 表示audio总时间
-			  	if(Atime == 0) {
-			  	Atime = minSecond($("#" + audio_id)[0].duration);
-				}	  			
-              $time_count.eq(x).html(minSecond(Ctime) + "/" + Atime);	
-	  		}
-            // 播放的时候进度条的宽度变化
-	  		$progress_bar.eq(x).css("width",(Pmax* 85 / 100  * Ctime / $("#" + audio_id)[0].duration) +'px');
-	  		// 播放时候小圆点的移动
-	  	    $yuan.eq(x).css("left", (Pmax * 85 / 100 * Ctime / $("#" + audio_id)[0].duration + 3)+"px");
-	  	},1000)
-	 	    // 储存当前audio的id作为下一次点击的判断
-	        pre_audio = $("#" + audio_id)[0];
-	        //获得当前audio的下标作为下一次点击的按钮下标
-	        pre_index = x;	  	
-  	} else {
-  		// 当点击按钮的时候如果按钮是暂停图标，就改成播放图标
-	   	$(this).removeClass('glyphicon-pause').addClass('glyphicon-play');	
-  		// 当前audio暂停	
-	  	$("#" + audio_id)[0].pause();  		
-  	}
-  })//按钮的click function
-}//最大的for
+
+// 注意！获取宽度，如果是非内联css样式，只能用offsetWidth
+//进度条的最大长度，整个框架x55%
+var Pmax = $song[0].offsetWidth * 55 /100;
+// 当点击播放按钮时
+$btn_audio.on("click", function () {
+	//用来表示audio的总时间
+   var Atime = 0; 	
+	// 获取当前节点data-index属性值，和对应audio的ID一样
+	var audio_id = $(this).attr('data-index');
+	var x = audio_id.match(/\d/);
+// 点击时判断是否是三角形播放按钮
+	if ($(this).hasClass('glyphicon-play') ) {
+   	//让圆点显示出来
+   	$yuan.eq(x).css("display","block");
+    // 判断上一个点击的audio，让其暂停，并改变按钮图标
+    if (pre_audio!=undefined) {
+    	pre_audio.pause();      	
+    	$btn_audio.eq(pre_index).removeClass('glyphicon-pause').addClass('glyphicon-play');
+    }
+    //当前符合条件的audio按钮背景换成暂停
+   	$(this).removeClass('glyphicon-play').addClass('glyphicon-pause');
+	//当前符合条件的audio播放
+  	$("#" + audio_id)[0].play();	
+    //设置定时器，
+  	var a = setInterval(function(){
+  		//当audio暂停的时候，清除定时器
+  		if ($("#" + audio_id).paused){
+  			clearInterval(a);
+  		}
+  		//定义当前audio已播放时间
+       var Ctime = $("#" + audio_id)[0].currentTime;
+  		// 让(class为time_count)p标签内容变成当前播放时间
+  		if (Ctime > 0.01) {
+		  	//Atime 表示audio总时间
+		  	if(Atime == 0) {
+		  	Atime = minSecond($("#" + audio_id)[0].duration);
+			}	  			
+          $time_count.eq(x).html(minSecond(Ctime) + "/" + Atime);	
+  		}
+        // 播放的时候进度条的宽度变化
+  		$progress_bar.eq(x).css("width",(Pmax* 85 / 100  * Ctime / $("#" + audio_id)[0].duration) +'px');
+  		// 播放时候小圆点的移动
+  	    $yuan.eq(x).css("left", (Pmax * 85 / 100 * Ctime / $("#" + audio_id)[0].duration + 3)+"px");
+  	},1000)
+ 	    // 储存当前audio的id作为下一次点击的判断
+        pre_audio = $("#" + audio_id)[0];
+        //获得当前audio的下标作为下一次点击的按钮下标
+        pre_index = x;	  	
+	} else {
+		// 当点击按钮的时候如果按钮是暂停图标，就改成播放图标
+   	$(this).removeClass('glyphicon-pause').addClass('glyphicon-play');	
+		// 当前audio暂停	
+  	$("#" + audio_id)[0].pause();  		
+	}
+})//按钮的click function
+
 
 
 //把秒数转换才分钟
